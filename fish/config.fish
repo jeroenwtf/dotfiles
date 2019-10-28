f512164d74c9eb0d822425b3d5a1771e1eabb6c9
Jeroen van Meerendonk <hola@jeroen.wtf>
2019-10-28 20:00:39 +0100

    Updates a couple of things of fish shell.

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,10 @@
-eval (python -m virtualfish)
+#eval (python -m virtualfish)
 thefuck --alias | source
 
 source $HOME/.dotfiles/fish/functions/aliases.fish
 source $HOME/.dotfiles/fish/functions/private_aliases.fish
+
+set -g -x fish_greeting ''
 
 # Spacefish options
 set SPACEFISH_PROMPT_ORDER time user dir host git pyenv exec_time line_sep battery jobs exit_code char
