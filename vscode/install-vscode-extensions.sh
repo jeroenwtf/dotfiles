e97b918f1869dd9364206a37d44bcb888306b783
Jeroen van Meerendonk <jeroen.vanmeerendonk@domestika.org>
2021-04-04 16:29:00 +0200

    Adds vscode and updates gitignore

---
+++ vscode/install-vscode-extensions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# execute command
+# -------------------
+# curl -s https://raw.githubusercontent.com/karaage0703/vscode-dotfiles/master/install-vscode-extensions.sh | /bin/bash
+
+# Visual Studio Code :: Package list
+pkglist=(
+amandeepmittal.pug
+bradlc.vscode-tailwindcss
+dbaeumer.vscode-eslint
+EditorConfig.EditorConfig
+esbenp.prettier-vscode
+karunamurti.haml
+ms-vsliveshare.vsliveshare
+rebornix.ruby
+sianglim.slim
+thibaudcolas.stylelint
+waderyan.gitblame
+wingrunr21.vscode-ruby
+zhuangtongfa.material-theme
+)
+
+for i in ${pkglist[@]}; do
+  code --install-extension $i
+done
