3056017d3ceb873969e7ceda222ab282563ce14b
Jeroen van Meerendonk <hola@jeroen.wtf>
2021-06-22 18:34:02 +0200

    Adds config for rofi and some tinkering

--- fish/config.fish
+++ fish/config.fish
@@ -3,10 +3,18 @@ thefuck --alias | source
 source $HOME/.dotfiles/fish/functions/aliases.fish
 
 # Makes npm work or something
-set PATH /home/jeroen/.nvm/versions/node/v14.16.0/bin $PATH
+function nvm
+    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+end
+
+set -x NVM_DIR ~/.nvm
+nvm use default --silent
 
 # Removes fish greeting
 set -g -x fish_greeting ''
 
 # Spacefish options
 set SPACEFISH_PROMPT_ORDER time user dir host git pyenv exec_time line_sep battery jobs exit_code char
+
+set PATH /home/jeroen/.rvm/bin $PATH
+rvm default
