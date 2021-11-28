ef2bfc851004f9b5e59685ff17b0276e04282fd1
Jeroen van Meerendonk <hola@jeroen.wtf>
2021-11-28 17:19:11 +0100

    Changes gitignore entries for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 thefuck --alias | source
 
-source $HOME/.dotfiles/fish/functions/aliases.fish
+source $HOME/.dotfiles/fish/aliases.fish
 
 # Makes npm work or something
 function nvm
@@ -12,9 +12,6 @@ nvm use default --silent
 
 # Removes fish greeting
 set -g -x fish_greeting ''
-
-# Spacefish options
-set SPACEFISH_PROMPT_ORDER time user dir host git pyenv exec_time line_sep battery jobs exit_code char
 
 set PATH /home/jeroen/.rvm/bin $PATH
 rvm default
