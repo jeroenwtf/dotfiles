2829a6f7990d9eec1fef9ffaa4fe9ce2c4eee6fc
Jeroen van Meerendonk <hola@jeroen.wtf>
2023-09-29 13:18:10 +0200

    Add ruby stuff in neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -301,8 +301,8 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'css', 'go', 'javascript', 'lua', 'python', 'rust', 'scss', 'tsx', 'typescript',
-    'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'css', 'go', 'javascript', 'lua', 'python', 'ruby', 'rust', 'scss', 'tsx',
+    'typescript', 'vimdoc', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
@@ -433,6 +433,7 @@ local servers = {
   -- pyright = {},
   -- rust_analyzer = {},
   tsserver = {},
+  standardrb = {},
   stylelint_lsp = {
     settings = {
       stylelintplus = {
