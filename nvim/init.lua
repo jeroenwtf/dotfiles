246f8721a36924e7d1fce457c6d83647d8d41df3
Jeroen van Meerendonk <hola@jeroen.wtf>
2024-07-17 17:34:10 +0200

    Add mini.move

--- nvim/init.lua
+++ nvim/init.lua
@@ -583,6 +583,7 @@ require('lazy').setup({
 
       require('mini.bufremove').setup()
       require('mini.tabline').setup()
+      require('mini.move').setup()
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
