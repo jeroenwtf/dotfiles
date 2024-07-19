fca9a757d9e72f2917c65d28947b99e2d7a78b82
Jeroen van Meerendonk <hola@jeroen.wtf>
2024-07-19 17:45:41 +0200

    Show hidden files and add conflict plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -154,6 +154,10 @@ require('lazy').setup({
     config = function()
       require('neo-tree').setup {
         filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            never_show = { '.git' },
+          },
           follow_current_file = {
             enabled = true,
           },
@@ -166,6 +170,31 @@ require('lazy').setup({
     'windwp/nvim-ts-autotag',
     config = function()
       require('nvim-ts-autotag').setup()
+    end,
+  },
+
+  {
+    'akinsho/git-conflict.nvim',
+    version = '*',
+    config = function()
+      require('git-conflict').setup {
+        default_mappings = false,
+        default_commands = true,
+        disable_diagnostics = false,
+        list_opener = 'copen',
+        highlights = {
+          incoming = 'DiffAdd',
+          current = 'DiffText',
+        },
+        debug = false,
+      }
+
+      vim.keymap.set('n', 'xo', '<Plug>(git-conflict-ours)')
+      vim.keymap.set('n', 'xt', '<Plug>(git-conflict-theirs)')
+      vim.keymap.set('n', 'xb', '<Plug>(git-conflict-both)')
+      vim.keymap.set('n', 'x0', '<Plug>(git-conflict-none)')
+      vim.keymap.set('n', 'xp', '<Plug>(git-conflict-prev-conflict)')
+      vim.keymap.set('n', 'xn', '<Plug>(git-conflict-next-conflict)')
     end,
   },
 
