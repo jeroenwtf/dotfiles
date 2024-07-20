abe3d9f9484b39e0b8792568fcde873eecdfa352
Jeroen van Meerendonk <hola@jeroen.wtf>
2024-07-20 16:07:37 +0200

    Redo basic keymap and add super cool git add shit

--- nvim/init.lua
+++ nvim/init.lua
@@ -244,6 +244,8 @@ require('lazy').setup({
           { '<leader>d', group = '[D]ocument' },
           { '<leader>r', group = '[R]ename' },
           { '<leader>s', group = '[S]earch' },
+          { '<leader>t', group = '[T]oggle' },
+          { '<leader>x', group = '[X] Git conflict' },
           { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
         },
       }
@@ -300,7 +302,7 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>t', builtin.find_files, { desc = 'Search files [t]' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
