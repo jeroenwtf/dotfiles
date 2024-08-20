a8bafbe7916ed86e5b8b7bddb675cb7c46108ef8
Jeroen van Meerendonk <hola@jeroen.wtf>
2024-08-20 15:39:12 +0200

    Refresh neo-tree when using lazygit

--- nvim/init.lua
+++ nvim/init.lua
@@ -349,6 +349,15 @@ require('lazy').setup({
     keys = {
       { '<leader>g', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
+    config = function()
+      vim.api.nvim_create_autocmd({ 'BufLeave' }, {
+        pattern = { '*lazygit*' },
+        group = vim.api.nvim_create_augroup('git_refresh_neotree', { clear = true }),
+        callback = function()
+          require('neo-tree.sources.filesystem.commands').refresh(require('neo-tree.sources.manager').get_state 'filesystem')
+        end,
+      })
+    end,
   },
 
   { -- LSP Configuration & Plugins
