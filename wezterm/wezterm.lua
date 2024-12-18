403b033cba5130f24a5f4447e065d7d68b561d80
Jeroen van Meerendonk <hola@jeroen.wtf>
2024-12-18 13:19:52 +0100

    Add Wezterm config again

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 12
+config.line_height = 1.2
+config.color_scheme = "Tokyo Night"
+
+config.keys = {
+	{
+		key = "Enter",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+}
+
+config.window_padding = {
+	left = 20,
+	right = 20,
+	top = 20,
+	bottom = 20,
+}
+
+return config
