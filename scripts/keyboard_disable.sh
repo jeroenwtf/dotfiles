7607dff73ae9be5eb7c68330bc228624b74b8f79
Jeroen van Meerendonk <hola@jeroen.wtf>
2024-12-19 01:09:36 +0100

    Remove sudo from keyboard toggling script

--- scripts/keyboard_disable.sh
+++ scripts/keyboard_disable.sh
@@ -2,7 +2,7 @@
 
 DEVICE_NAME="Framework Laptop 16 Keyboard Module - ANSI Keyboard"
 
-echo 'KERNEL=="event*", ATTRS{name}=="'"$DEVICE_NAME"'", ENV{LIBINPUT_IGNORE_DEVICE}="1"' | sudo tee /etc/udev/rules.d/10-disable-internal-keyboard.rules > /dev/null
+echo 'KERNEL=="event*", ATTRS{name}=="'"$DEVICE_NAME"'", ENV{LIBINPUT_IGNORE_DEVICE}="1"' | tee /etc/udev/rules.d/10-disable-internal-keyboard.rules > /dev/null
 
-sudo udevadm control --reload-rules
-sudo udevadm trigger
+udevadm control --reload-rules
+udevadm trigger
