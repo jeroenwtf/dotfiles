88e1222574d8515e9511b4e5a43b0915924e9936
Jeroen van Meerendonk <hola@jeroen.wtf>
2025-01-13 18:08:33 +0100

    Changes

--- fish/config.fish
+++ fish/config.fish
@@ -20,6 +20,10 @@ fish_add_path ~/.local/bin
 # fish_add_path ~/.local/share/mise/shims
 
 # Sources
-mise activate fish | source
+if status is-interactive
+  mise activate fish | source
+else
+  mise activate fish --shims | source
+end
 thefuck --alias | source
 starship init fish | source
