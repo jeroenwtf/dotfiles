9395b94c1e29ba340770495b062349bd045a1bdb
Jeroen van Meerendonk <hola@jeroen.wtf>
2025-02-03 18:56:08 +0100

    Try to fix something

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -24,5 +24,14 @@ return {
     { '<leader>w', function() Snacks.bufdelete() end,     desc = 'Close current buffer' },
     { '<leader>W', function() Snacks.bufdelete.all() end, desc = 'Close all buffers' },
     { '<leader>g', function() Snacks.lazygit() end,       desc = 'Open lazy[g]it' }
-  }
+  },
+  init = function()
+    vim.api.nvim_create_autocmd({ 'BufLeave' }, {
+      pattern = { '*lazygit*' },
+      group = vim.api.nvim_create_augroup('git_refresh_neotree', { clear = true }),
+      callback = function()
+        require('neo-tree.sources.filesystem.commands').refresh(require('neo-tree.sources.manager').get_state 'filesystem')
+      end,
+    })
+  end
 }
