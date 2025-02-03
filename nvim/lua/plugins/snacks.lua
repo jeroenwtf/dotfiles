8b9503d1b970ed883b19ac380a3a26e0902446ff
Jeroen van Meerendonk <hola@jeroen.wtf>
2025-02-03 19:43:50 +0100

    Mneeeeeeh

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,7 +2,6 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  ---@type snacks.Config
   opts = {
     indent = {
       enabled = true,
@@ -21,17 +20,8 @@ return {
     },
   },
   keys = {
-    { '<leader>w', function() Snacks.bufdelete() end,     desc = 'Close current buffer' },
-    { '<leader>W', function() Snacks.bufdelete.all() end, desc = 'Close all buffers' },
-    { '<leader>g', function() Snacks.lazygit() end,       desc = 'Open lazy[g]it' }
+    { "<leader>w", function() Snacks.bufdelete() end,     desc = "Close current buffer" },
+    { "<leader>W", function() Snacks.bufdelete.all() end, desc = "Close all buffers" },
+    { "<leader>g", function() Snacks.lazygit() end,       desc = "Open lazy[g]it" },
   },
-  init = function()
-    vim.api.nvim_create_autocmd({ 'BufLeave' }, {
-      pattern = { '*lazygit*' },
-      group = vim.api.nvim_create_augroup('git_refresh_neotree', { clear = true }),
-      callback = function()
-        require('neo-tree.sources.filesystem.commands').refresh(require('neo-tree.sources.manager').get_state 'filesystem')
-      end,
-    })
-  end
 }
