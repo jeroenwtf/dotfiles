6f7689d6c7a67d268d75aa8788d3f3578d669a07
Jeroen van Meerendonk <hola@jeroen.wtf>
2025-02-03 19:48:38 +0100

    Mneeeeeeeeeeh again

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -43,6 +43,12 @@ end, { desc = 'Open floating menu' })
 -- neo-tree
 vim.keymap.set('n', '<C-s>', '<cmd>Neotree document_symbols reveal right<CR>',
   { desc = 'Toggle document [s]ymbols sidebar' })
+-- Add a keymap for <C-r> to refresh the git status of neotree
+vim.keymap.set('n', '<C-r>', function()
+    require('neo-tree.sources.filesystem.commands').refresh(require('neo-tree.sources.manager').get_state 'filesystem')
+    print("Neotree git status refreshed")
+  end,
+  { desc = 'Refresh git status in Neotree' })
 
 -- Diagnostic keymap
 vim.keymap.set('n', '<S-C-d>', function()
