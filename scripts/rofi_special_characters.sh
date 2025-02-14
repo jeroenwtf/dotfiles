7d3ca54207df25882240759ba99660b2eddadfde
Jeroen van Meerendonk <hola@jeroen.wtf>
2025-02-14 17:17:54 +0100

    Add rofi script to select special characters

---
+++ scripts/rofi_special_characters.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+declare -A characters
+
+characters=(
+    ["Mdash (—)"]="—"
+    ["Uppercase c trencada (Ç)"]="Ç"
+    ["Lowercase c trencada (ç)"]="ç"
+    ["Opening exclamation mark (¡)"]="¡"
+    ["Opening question mark (¿)"]="¿"
+    ["Euro (€)"]="€"
+    ["Catalan dot (·)"]="·"
+)
+
+selection=$(printf "%s\n" "${!characters[@]}" | rofi -dmenu -i -p "Pick a character:")
+
+if [[ -n "$selection" && -n "${characters[$selection]}" ]]; then
+    xdotool type "${characters[$selection]}"
+fi
