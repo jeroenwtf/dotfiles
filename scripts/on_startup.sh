621c0c6982265450a1e566992dc354e3aefb4b22
Jeroen van Meerendonk <hola@jeroen.wtf>
2025-02-20 09:26:16 +0100

    Update on_startup.sh

--- scripts/on_startup.sh
+++ scripts/on_startup.sh
@@ -4,9 +4,9 @@
 
 script_dir="$(dirname "$0")"
 
-"$script_dir/remap_trackball.sh"
-"$script_dir/weekday_apps.sh"
-
 if [ "$HOSTNAME" = "hyperion" ]; then
     "$script_dir/switch_main_display.sh"
 fi
+
+"$script_dir/remap_trackball.sh"
+"$script_dir/weekday_apps.sh"
